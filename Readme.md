d855a324e741b9f5703acc6497220b2b7ba2b433
许一涵 <81723334@qq.com>
2024-04-27 16:24:32 +0800

    docs:完善readme以及修复文档结构

--- Readme.md
+++ Readme.md
@@ -1,20 +1,58 @@
 # 北京交通大学最帅的小组
 需要克隆，请使用命令`git clone --recurse-submodules https://github.com/shixunIV/BackEnd.git`递归的克隆
-安卓端开发请在Android子目录
 Web端开发请在Web子目录下
-注意git提交规范！提交规范详见下面链接
->https://juejin.cn/post/6844903793033756680
+注意git提交规范！提交规范详见下面链接`https://juejin.cn/post/6844903793033756680`
 
 
 # 项目说明
 一个医疗知识图谱,可以基于知识图谱进行问答等内容.
 注意项目所有代码需要再BACKEND目录下运行!否则相对路径会有问题
+需求文档：`https://ux1cafcdniz.feishu.cn/docx/GTacdliJLo69P8xrdM8c7OK5nMb`
+
 
 # 系统需求
+## 配置相关
 需要安装python与npm/nodejs, 数据库采用sqlite,省去配置步骤,图数据库采用neo4j,用于构建数据库
 在config.yml中配置自己的neo4j数据库等本地信息
 
+## 运行须知
+电脑安装有golang python pnpm nodejs
+首先去web目录底下运行`pnpm i`下载依赖，然后去gateway下运行`go mod tidy`安装依赖
 
 # 目录结构
 前端实现在web目录下,不需要实现Android端,使用React进行开发,给我开发的好看一些@hkjgsfgh
+运行`python main.py`直接运行多个目录
+
+## gateway
+**用于转发请求**
+是防火墙，各个组件均需要屏蔽来自非gateway的请求（127.0.0.1）
+使用go撰写，主要的用处是转发，作为多个微服务组件的统一入口。
+
+## neo4j
+**注意使用中间件屏蔽非本地的访问**
+图数据库微服务，主要用处是进行图数据库的查询等操作。
+
+
+## neo4jInit
+图数据库构建脚本，用于构建图数据库，具体流程如下：
+通过多线程爬虫爬取寻医问药网站的所有疾病顺序：
+```python
+basic_url = "http://jib.xywy.com/il_sii/gaishu/%s.htm" % page  # 疾病描述
+cause_url = "http://jib.xywy.com/il_sii/cause/%s.htm" % page  # 疾病起因
+prevent_url = "http://jib.xywy.com/il_sii/prevent/%s.htm" % page  # 疾病预防
+symptom_url = "http://jib.xywy.com/il_sii/symptom/%s.htm" % page  # 疾病症状
+inspect_url = "http://jib.xywy.com/il_sii/inspect/%s.htm" % page  # 疾病检查
+treat_url = "http://jib.xywy.com/il_sii/treat/%s.htm" % page  # 疾病治疗
+food_url = "http://jib.xywy.com/il_sii/food/%s.htm" % page  # 饮食治疗
+```
+从网页中解析疾病相关数据，由于疾病是格式化数据，所以不需要过多的转换。爬取的数据全部存放在`data/spider_data`目录下以json格式存放。
+然后对数据进行清洗，相关代码在data_prepare中，主要使用jieba库将一些黏连的数据切分开以及格式化数据。
+导出所有的病例名称到disease.txt为了切割数据
+然后使用build_graph.py构建图数据库，时间大概需要一个小时，需要耐心等待
+
+## Patient 
+用与管理病人
+
+## doctor
+用于管理医生
 
--- neo4jInit/data_prepare.py
+++ neo4jInit/data_prepare.py
@@ -4,6 +4,7 @@ import jieba
 import concurrent.futures
 from py2neo import Graph, Node
 
+
 def process_file(i):
     path = "./neo4jInit/data/spider_data"
     target_path = "./neo4jInit/data/new_data/"
@@ -12,40 +13,44 @@ def process_file(i):
         data = json.load(f)
     # 需要的数据
     dict = {}
-    dict["名称"] = data["basic_info"]["name"]
-    dict["描述"] = data["basic_info"]["desc"]
-    dict["分类"] = data["basic_info"]["category"][1:-1]
-    dict["是否医保"] = (
+    dict["name"] = data["basic_info"]["name"]
+    dict["desc"] = data["basic_info"]["desc"]
+    dict["category"] = data["basic_info"]["category"][1:-1]
+    dict["health_insuranc"] = (
         data["basic_info"]["attributes"][0].split("：")[1].replace(" ", "")
     )
-    dict["患病比例"] = data["basic_info"]["attributes"][1].split("：")[1]
-    dict["易感人群"] = (
+    dict["disease_proportion"] = data["basic_info"]["attributes"][1].split("：")[1]
+    dict["susceptible_population"] = (
         data["basic_info"]["attributes"][2].split("：")[1].replace(" ", "")
     )
-    dict["传染方式"] = data["basic_info"]["attributes"][3].split("：")[1]
+    dict["mode_of_infection"] = data["basic_info"]["attributes"][3].split("：")[1]
     sym = data["basic_info"]["attributes"][4].split("：")[1]
     words = jieba.lcut(sym)
-    dict["并发症"] = words
+    dict["complications"] = words
     seg_list = jieba.lcut(
         data["basic_info"]["attributes"][5].split("：")[1], cut_all=False
     )
     seg_list = [x for x in seg_list if x != " "]
-    dict["就诊科室"] = seg_list
-    dict["治疗方式"] = data["basic_info"]["attributes"][6].split("：")[1].split(" ")
-    dict["治疗周期"] = data["basic_info"]["attributes"][7].split("：")[1]
-    dict["治愈率"] = data["basic_info"]["attributes"][8].split("：")[1].replace(" ", "")
-    dict["治疗费用"] = data["basic_info"]["attributes"][9].split("：")[1]
-    dict["推荐"] = data["basic_info"]["attributes"][10]
-    dict["原因"] = data["cause_info"]
-    dict["预防"] = data["prevent_info"]
-    dict["症状"] = data["symptom_info"][0]
-    dict["具体表现"] = data["symptom_info"][1]
-    dict["检查"] = data["inspect_info"]
-    dict["饮食保健"] = data["food_info"]["good"]
-    dict["忌吃食物"] = data["food_info"]["bad"]
-    dict["推荐食物"] = data["food_info"]["recommand"]
-    dict["常用药品"] = data["common_drug"]
-    dict["药品明细"] = data["drug_detail"]
+    dict["medical_department"] = seg_list
+    dict["mode_of_treatment"] = (
+        data["basic_info"]["attributes"][6].split("：")[1].split(" ")
+    )
+    dict["treatment_cycle"] = data["basic_info"]["attributes"][7].split("：")[1]
+    dict["cure_rate"] = (
+        data["basic_info"]["attributes"][8].split("：")[1].replace(" ", "")
+    )
+    dict["treatment_cost"] = data["basic_info"]["attributes"][9].split("：")[1]
+    dict["recommend"] = data["basic_info"]["attributes"][10]
+    dict["cause"] = data["cause_info"]
+    dict["prevention"] = data["prevent_info"]
+    dict["symptom"] = data["symptom_info"][0]
+    dict["concrete_performance"] = data["symptom_info"][1]
+    dict["inspect"] = data["inspect_info"]
+    dict["diet_and_health_care"] = data["food_info"]["good"]
+    dict["avoid_eating_food"] = data["food_info"]["bad"]
+    dict["recommend_food"] = data["food_info"]["recommand"]
+    dict["commonly_used_drugs"] = data["common_drug"]
+    dict["drug_detail"] = data["drug_detail"]
     save_path = os.path.join(target_path, str(i) + ".json")
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
     with open(save_path, "w", encoding="utf-8") as f:
@@ -73,9 +78,6 @@ def get_entity():
     dict["食物"] = set()
     dict["药品"] = set()
     dict["细分药品"] = set()
-
-
-
 
 
 if __name__ == "__main__":
