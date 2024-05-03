8bb0d0ebbe12a3688b033fdde7b7f9401fceb8a9
许一涵 <81723334@qq.com>
2024-05-03 19:57:59 +0800

    init

--- neo4jInit/data_prepare.py
+++ neo4jInit/data_prepare.py
@@ -1,85 +1,24 @@
 import os
 import json
-import jieba
+from gpt import GPT, read_config
 import concurrent.futures
-from py2neo import Graph, Node
 
 
-def process_file(i):
-    path = "./neo4jInit/data/spider_data"
-    target_path = "./neo4jInit/data/new_data/"
-    # 载入path
-    with open(os.path.join(path, str(i) + ".json"), "r", encoding="utf-8") as f:
-        data = json.load(f)
-    # 需要的数据
-    dict = {}
-    dict["name"] = data["basic_info"]["name"]
-    dict["desc"] = data["basic_info"]["desc"]
-    dict["category"] = data["basic_info"]["category"][1:-1]
-    dict["health_insuranc"] = (
-        data["basic_info"]["attributes"][0].split("：")[1].replace(" ", "")
+def process_data(i, data):
+    ans = gpt.ask(
+        "这是 列车组/乘客/环境/设备 造成的事故?"
+        + json.dumps(data[i], ensure_ascii=False)
+        + "你只需要回答列车组/乘客/环境/设备即可"
     )
-    dict["disease_proportion"] = data["basic_info"]["attributes"][1].split("：")[1]
-    dict["susceptible_population"] = (
-        data["basic_info"]["attributes"][2].split("：")[1].replace(" ", "")
-    )
-    dict["mode_of_infection"] = data["basic_info"]["attributes"][3].split("：")[1]
-    sym = data["basic_info"]["attributes"][4].split("：")[1]
-    words = jieba.lcut(sym)
-    dict["complications"] = words
-    seg_list = jieba.lcut(
-        data["basic_info"]["attributes"][5].split("：")[1], cut_all=False
-    )
-    seg_list = [x for x in seg_list if x != " "]
-    dict["medical_department"] = seg_list
-    dict["mode_of_treatment"] = (
-        data["basic_info"]["attributes"][6].split("：")[1].split(" ")
-    )
-    dict["treatment_cycle"] = data["basic_info"]["attributes"][7].split("：")[1]
-    dict["cure_rate"] = (
-        data["basic_info"]["attributes"][8].split("：")[1].replace(" ", "")
-    )
-    dict["treatment_cost"] = data["basic_info"]["attributes"][9].split("：")[1]
-    dict["recommend"] = data["basic_info"]["attributes"][10]
-    dict["cause"] = data["cause_info"]
-    dict["prevention"] = data["prevent_info"]
-    dict["symptom"] = data["symptom_info"][0]
-    dict["concrete_performance"] = data["symptom_info"][1]
-    dict["inspect"] = data["inspect_info"]
-    dict["diet_and_health_care"] = data["food_info"]["good"]
-    dict["avoid_eating_food"] = data["food_info"]["bad"]
-    dict["recommend_food"] = data["food_info"]["recommand"]
-    dict["commonly_used_drugs"] = data["common_drug"]
-    dict["drug_detail"] = data["drug_detail"]
-    save_path = os.path.join(target_path, str(i) + ".json")
+    save_path = os.path.join("neo4jInit/data/new_data", f"railway_{i}.json")
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
-    with open(save_path, "w", encoding="utf-8") as f:
-        json.dump(dict, f, ensure_ascii=False, indent=4)
-
-
-def prepare_data():
-    path = "./neo4jInit/data/disease.txt"
-    with open(path, "r", encoding="utf-8") as f:
-        sympotom = [line.strip() for line in f]
-    jieba.load_userdict(sympotom)
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(process_file, range(1, 10138))
-
-
-def get_entity():
-    dict = {}
-    dict["名称"] = set()
-    dict["分类"] = set()  # 去掉第一个疾病百科和最后一个重复名称
-    dict["就诊科室"] = set()
-    dict["治疗方式"] = set()
-    dict["传染方式"] = set()
-    dict["症状"] = set()
-    dict["检查"] = set()
-    dict["食物"] = set()
-    dict["药品"] = set()
-    dict["细分药品"] = set()
+    data[i]["列车组/乘客/环境/设备"] = ans[:2]
+    json.dump(data[i], open(save_path, "w"), ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
-    prepare_data()
-    # create_diseases()
+    data = json.load(open("neo4jInit/data/railway.json"))
+    config = read_config("./config.yml")
+    gpt = GPT(config)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(process_data, range(len(data)), [data] * len(data))
---
+++ neo4jInit/gpt.py
@@ -0,0 +1,141 @@
+from openai import OpenAI
+import re
+import yaml
+
+# TODO!待修改的提示词
+prompt = """
+你现在是一个neo4j数据库查询高手
+# 所有实体结构：
+```python
+Node(
+	"illness",
+	name["名称"],
+	desc["描述"],
+	medical_insurance["是否医保"],
+	disease_proportion["患病比例"],
+	susceptible_population["易感人群"],
+	treatment_cycle["治疗周期"],
+	cure_rate["治愈率"],
+	treatment_cost["治疗费用"],
+	disease_tips["推荐"],
+	reason["原因"],
+	prevention["预防"],
+	concrete_performance["具体表现"],
+)
+
+Node("food", name["食品名称"])
+
+Node("commonly_used_drugs", name["药品名称"])
+
+Node("details_of_drugs", name["药品名称"])
+
+Node("category", name["疾病分类"])
+
+Node("medical_department", name["就诊科室"])
+
+Node("treatment_mode", name["治疗方式"]
+
+Node("symptom", name["症状"])	
+
+Node("inspection_mode", name["检查方式"])
+
+Node("infection_mode", name["传染方式"]) 
+```
+# 关系三元组
+
+[illness belong category]
+[illness medical_department medical_department]
+[illness treatment_mode treatment_mode]
+[illness infection_mode infection_mode]
+[illness symptom symptom]
+[illness inspection_mode inspection_mode]
+[illness diet_and_health_care food]
+[illness avoid_eating_food food]
+[illness recommend_food food]
+[illness commonly_used_drugs commonly_used_drugs]
+[illness details_of_drugs details_of_drugs]
+[illness compliation illness]
+# 要求
+请你根据我接下来的问题给出对应的neo4j查询语句，neo4j语句需包含在```cypher```代码块中
+"""
+
+
+def read_config(file_path):
+    with open(file_path, "r") as file:
+        config_data = yaml.safe_load(file)
+    return config_data
+
+
+class GPT:
+    def __init__(self, config) -> None:
+        self.client = OpenAI(
+            api_key=config["openai"]["api_key"],
+        )
+
+    def generate_sql(self, question):
+        global prompt
+        response = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": prompt},
+                {
+                    "role": "assistant",
+                    "content": """
+    Sure, I can help you with that. Just let me know what specific queries you need for Neo4j, and I'll provide you with the corresponding Cypher queries.""",
+                },
+                {
+                    "role": "user",
+                    "content": question,
+                },
+            ],
+        )
+        # 使用正则匹配出cypher语句
+        pattern = r"```cypher(.*?)```"
+        match = re.search(
+            pattern, response.choices[0].message.content.strip(), re.DOTALL
+        )
+        if match:
+            cypher_query = match.group(1).strip()
+            return cypher_query
+        else:
+            return "出错啦！"
+
+    def generate_ans(self, question, ans):
+        response = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "user",
+                    "content": "你现在是一个医生，请你根据我的问题和根据该问题从数据库中查询出来的结果组织成人类语言返回",
+                },
+                {
+                    "role": "assistant",
+                    "content": "当然，请问你有什么健康问题需要咨询？",
+                },
+                {
+                    "role": "user",
+                    "content": f"问题:{question}  查询结果{ans}",
+                },
+            ],
+        )
+        print(f"问题:{question}  查询结果{ans}")  #
+        return response.choices[0].message.content.strip()
+
+    def ask(self, question):
+        response = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "user",
+                    "content": question,
+                },
+            ],
+        )
+        return response.choices[0].message.content.strip()
+
+
+if __name__ == "__main__":
+    config = read_config("./config.yml")
+    gpt = GPT(config)
+    # print(gpt.generate_sql("阳痿吃什么"))
+    gpt.ask("阳痿吃什么")
