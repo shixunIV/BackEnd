99ce5adc555fa8cb86e24e556810c364d6c111e6
许一涵 <81723334@qq.com>
2024-05-03 22:38:20 +0800

    feat:图数据库构建

--- neo4jInit/build_graph.py
+++ neo4jInit/build_graph.py
@@ -2,6 +2,7 @@ import json
 from py2neo import Graph, Node
 import os
 import yaml
+from py2neo import Relationship
 
 
 def read_config(file_path):
@@ -12,7 +13,8 @@ def read_config(file_path):
 
 class MedicalGraph:
     def __init__(self):
-        self.data_path = "./neo4jInit/data/medical.json"
+        self.data_path = "./neo4jInit/data/new_data"
+        self.data = self.read_nodes()
         config = read_config("config.yml")
         self.g = Graph(
             host="127.0.0.1",
@@ -20,298 +22,169 @@ class MedicalGraph:
             user=config["neo4j"]["user"],
             password=str(config["neo4j"]["password"]),
         )
-
-    def read_data(self):
-        with open(self.data_path, "r", encoding="utf-8") as f:
-            data = json.load(f)
+    def read_nodes(self):
+        data = []
+        for file in os.listdir(self.data_path):
+            with open(os.path.join(self.data_path, file), "r", encoding="utf-8") as f:
+                data.append(json.load(f))
         return data
+    
+    def create_time_node(self):
+        time = set()
+        for item in self.data:
+            time.add(item["日期"])
+        for item in time:
+            node = Node("time", name=item)
+            self.g.create(node)
 
-    def create_diseases(self):
-        path = "./neo4jInit/data/new_data"
-        # 遍历这个文件夹
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            # 疾病节点的属性有 名称，描述，是否医保，患病比例，易感人群,治疗周期,治愈率,治疗费用,推荐,原因,预防,具体表现
-            self.g.create(
-                Node(
-                    "illness",
-                    name=json_data["名称"],
-                    desc=json_data["描述"][0],
-                    medical_insurance=json_data["是否医保"],
-                    disease_proportion=json_data["患病比例"],
-                    susceptible_population=json_data["易感人群"],
-                    treatment_cycle=json_data["治疗周期"],
-                    cure_rate=json_data["治愈率"],
-                    treatment_cost=json_data["治疗费用"],
-                    disease_tips=json_data["推荐"],
-                    reason=json_data["原因"],
-                    prevention=json_data["预防"],
-                    concrete_performance=json_data["具体表现"][0],
-                )
+    def create_route_node(self):
+        road = set()
+        for item in self.data:
+            road.add(item["路线"])
+        for item in road:
+            node = Node("route", name=item)
+            self.g.create(node)
+
+    def create_place_node(self):
+        place = set()
+        for item in self.data:
+            place.add(item["地点"])
+        for item in place:
+            node = Node("place", name=item)
+            self.g.create(node)
+
+    def create_checi_node(self):
+        checi = set()
+        for item in self.data:
+            checi.add(item["车次"])
+        for item in checi:
+            node = Node("train_number", name=item)
+            self.g.create(node)
+
+    def create_事故类型_node(self):
+        accident_type = set()
+        for item in self.data:
+            accident_type.add(item["事故类型"])
+        for item in accident_type:
+            node = Node("accident_type", name=item)
+            self.g.create(node)
+
+    def create_详细原因_node(self):
+        detail_reason = set()
+        for item in self.data:
+            detail_reason.add(item["原因"])
+        for item in detail_reason:
+            node = Node("detail_reason", name=item)
+            self.g.create(node)
+
+    def create_原因类型_node(self):
+        reason_type = set()
+        for item in self.data:
+            reason_type.add(item["列车组/乘客/环境/设备"])
+        for item in reason_type:
+            node = Node("reason_type", name=item)
+            self.g.create(node)
+
+    def create_Main_node(self):
+        i = 0
+        for item in self.data:
+            i += 1
+            node = Node(
+                "accident",
+                index=i,
+                death_toll=item["死亡人数"],
+                injured_toll=item["受伤人数"],
             )
-
-    def create_food(self):
-        path = "./neo4jInit/data/new_data"
-        set_food = set()
-        # 遍历这个文件夹
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            # 食物节点的属性有 饮食保健，忌吃食物，推荐食物,添加到set_food中
-            for tag in ["饮食保健", "忌吃食物", "推荐食物"]:
-                for food in json_data[tag]:
-                    set_food.add(food)
-
-        for food in set_food:
-            self.g.create(Node("food", name=food))
-
-    def create_drug(self):
-        path = "./neo4jInit/data/new_data"
-        set_drug_常用 = set()
-        set_drug_detail = set()
-        # 遍历这个文件夹
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            # 药品节点的属性有 常用药品，药品明细
-            for drug in json_data["常用药品"]:
-                set_drug_常用.add(drug)
-
-            for drug in json_data["药品明细"]:
-                set_drug_detail.add(drug)
-
-        for drug in set_drug_常用:
-            self.g.create(Node("commonly_used_drugs", name=drug))
-
-        for drug in set_drug_detail:
-            self.g.create(Node("details_of_drugs", name=drug))
-
-    def create_分类(self):
-        path = "./neo4jInit/data/new_data"
-        set_分类 = set()
-        # 遍历这个文件夹
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 分类 in json_data["分类"]:
-                set_分类.add(分类)
-
-        for tag in set_分类:
-            self.g.create(Node("category", name=tag))
-
-    def create_科室(self):
-        path = "./neo4jInit/data/new_data"
-        set_科室 = set()
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 就诊科室 in json_data["就诊科室"]:
-                set_科室.add(就诊科室)
-
-        for tag in set_科室:
-            self.g.create(Node("medical_department", name=tag))
-
-    def create_治疗方式(self):
-        path = "./neo4jInit/data/new_data"
-        set_治疗方式 = set()
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 治疗方式 in json_data["治疗方式"]:
-                set_治疗方式.add(治疗方式)
-
-        for tag in set_治疗方式:
-            self.g.create(Node("treatment_mode", name=tag))
-
-    def create_症状(self):
-        path = "./neo4jInit/data/new_data"
-        set_症状 = set()
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 症状 in json_data["症状"]:
-                set_症状.add(症状)
-
-        for tag in set_症状:
-            self.g.create(Node("symptom", name=tag))
-
-    def create_检查(self):
-        path = "./neo4jInit/data/new_data"
-        set_检查 = set()
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 检查 in json_data["检查"]:
-                set_检查.add(检查)
-
-        for tag in set_检查:
-            self.g.create(Node("inspection_mode", name=tag))
-
-    def create_传染方式(self):
-        path = "./neo4jInit/data/new_data"
-        set_传染方式 = set()
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 传染方式 in json_data["传染方式"]:
-                set_传染方式.add(传染方式)
-
-        for tag in set_传染方式:
-            self.g.create(Node("infection_mode", name=tag))
+            self.g.create(node)
 
     def create_entity(self):
         create_methods = [
-            self.create_diseases,
-            self.create_food,
-            self.create_drug,
-            self.create_分类,
-            self.create_科室,
-            self.create_治疗方式,
-            self.create_症状,
-            self.create_检查,
-            self.create_传染方式,
+            self.create_time_node,
+            self.create_route_node,
+            self.create_place_node,
+            self.create_checi_node,
+            self.create_事故类型_node,
+            self.create_详细原因_node,
+            self.create_原因类型_node,
+            self.create_Main_node,
         ]
         for method in create_methods:
             method()
 
-    def create_疾病_to_分类(self):
-        path = "./neo4jInit/data/new_data"
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 分类 in json_data["分类"]:
-                self.g.run(
-                    "MATCH (a:illness),(b:category) WHERE a.name = $a AND b.name = $b CREATE (a)-[r:belong]->(b)",
-                    a=json_data["名称"],
-                    b=分类,
-                )
+    def create_time_relation(self):
+        i = 0
+        for item in self.data:
+            i += 1
+            node1 = self.g.nodes.match("time", name=item["日期"]).first()
+            node2 = self.g.nodes.match("accident", index=i).first()
+            relation = Relationship(node1, "occurrence_time", node2)
+            self.g.create(relation)
 
-    def create_疾病_to_科室(self):
-        path = "./neo4jInit/data/new_data"
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 就诊科室 in json_data["就诊科室"]:
-                self.g.run(
-                    "MATCH (a:illness),(b:medical_department) WHERE a.name = $a AND b.name = $b CREATE (a)-[r:medical_department]->(b)",
-                    a=json_data["名称"],
-                    b=就诊科室,
-                )
+    def create_route_relation(self):
+        i = 0
+        for item in self.data:
+            i += 1
+            node1 = self.g.nodes.match("route", name=item["路线"]).first()
+            node2 = self.g.nodes.match("accident", index=i).first()
+            relation = Relationship(node1, "occurrence_route", node2)
+            self.g.create(relation)
 
-    def create_疾病_to_治疗方式(self):
-        path = "./neo4jInit/data/new_data"
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 治疗方式 in json_data["治疗方式"]:
-                self.g.run(
-                    "MATCH (a:illness),(b:treatment_mode) WHERE a.name = $a AND b.name = $b CREATE (a)-[r:treatment_mode]->(b)",
-                    a=json_data["名称"],
-                    b=治疗方式,
-                )
+    def create_place_relation(self):
+        i = 0
+        for item in self.data:
+            i += 1
+            node1 = self.g.nodes.match("place", name=item["地点"]).first()
+            node2 = self.g.nodes.match("accident", index=i).first()
+            relation = Relationship(node1, "occurrence_place", node2)
+            self.g.create(relation)
 
-    def create_疾病_to_传染方式(self):
-        path = "./neo4jInit/data/new_data"
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 传染方式 in json_data["传染方式"]:
-                self.g.run(
-                    "MATCH (a:illness),(b:infection_mode) WHERE a.name = $a AND b.name = $b CREATE (a)-[r:infection_mode]->(b)",
-                    a=json_data["名称"],
-                    b=传染方式,
-                )
+    def create_checi_relation(self):
+        i = 0
+        for item in self.data:
+            i += 1
+            node1 = self.g.nodes.match("train_number", name=item["车次"]).first()
+            node2 = self.g.nodes.match("accident", index=i).first()
+            relation = Relationship(node1, "occurrence_train_number", node2)
+            self.g.create(relation)
 
-    def create_疾病_to_症状(self):
-        path = "./neo4jInit/data/new_data"
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 症状 in json_data["症状"]:
-                self.g.run(
-                    "MATCH (a:illness),(b:symptom) WHERE a.name = $a AND b.name = $b CREATE (a)-[r:symptom]->(b)",
-                    a=json_data["名称"],
-                    b=症状,
-                )
+    def create_事故类型_relation(self):
+        i = 0
+        for item in self.data:
+            i += 1
+            node1 = self.g.nodes.match("accident_type", name=item["事故类型"]).first()
+            node2 = self.g.nodes.match("accident", index=i).first()
+            relation = Relationship(node1, "occurrence_accident_type", node2)
+            self.g.create(relation)
 
-    def create_疾病_to_检查(self):
-        path = "./neo4jInit/data/new_data"
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 检查 in json_data["检查"]:
-                self.g.run(
-                    "MATCH (a:illness),(b:inspection_mode) WHERE a.name = $a AND b.name = $b CREATE (a)-[r:inspection_mode]->(b)",
-                    a=json_data["名称"],
-                    b=检查,
-                )
+    def create_详细原因_relation(self):
+        i = 0
+        for item in self.data:
+            i += 1
+            node1 = self.g.nodes.match("detail_reason", name=item["原因"]).first()
+            node2 = self.g.nodes.match("accident", index=i).first()
+            relation = Relationship(node1, "occurrence_detail_reason", node2)
+            self.g.create(relation)
 
-    def create_疾病_to_饮食保健(self):
-        path = "./neo4jInit/data/new_data"
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 饮食保健 in json_data["饮食保健"]:
-                self.g.run(
-                    "MATCH (a:illness),(b:food) WHERE a.name = $a AND b.name = $b CREATE (a)-[r:diet_and_health_care]->(b)",
-                    a=json_data["名称"],
-                    b=饮食保健,
-                )
-
-    def create_疾病_to_忌吃食物(self):
-        path = "./neo4jInit/data/new_data"
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 忌吃食物 in json_data["忌吃食物"]:
-                self.g.run(
-                    "MATCH (a:illness),(b:food) WHERE a.name = $a AND b.name = $b CREATE (a)-[r:avoid_eating_food]->(b)",
-                    a=json_data["名称"],
-                    b=忌吃食物,
-                )
-
-    def create_疾病_to_推荐食物(self):
-        path = "./neo4jInit/data/new_data"
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 推荐食物 in json_data["推荐食物"]:
-                self.g.run(
-                    "MATCH (a:illness),(b:food) WHERE a.name = $a AND b.name = $b CREATE (a)-[r:recommend_food]->(b)",
-                    a=json_data["名称"],
-                    b=推荐食物,
-                )
-
-    def create_疾病_to_常用药品(self):
-        path = "./neo4jInit/data/new_data"
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 常用药品 in json_data["常用药品"]:
-                self.g.run(
-                    "MATCH (a:illness),(b:commonly_used_drugs) WHERE a.name = $a AND b.name = $b CREATE (a)-[r:commonly_used_drugs]->(b)",
-                    a=json_data["名称"],
-                    b=常用药品,
-                )
-
-    def create_疾病_to_药品明细(self):
-        path = "./neo4jInit/data/new_data"
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 药品明细 in json_data["药品明细"]:
-                self.g.run(
-                    "MATCH (a:illness),(b:details_of_drugs) WHERE a.name = $a AND b.name = $b CREATE (a)-[r:details_of_drugs]->(b)",
-                    a=json_data["名称"],
-                    b=药品明细,
-                )
-
-    def create_疾病_to_并发症(self):
-        path = "./neo4jInit/data/new_data"
-        for file in os.listdir(path):
-            json_data = json.load(open(os.path.join(path, file), "r", encoding="utf-8"))
-            for 并发症 in json_data["并发症"]:
-                self.g.run(
-                    "MATCH (a:illness),(b:illness) WHERE a.name = $a AND b.name = $b CREATE (a)-[r:complication]->(b)",
-                    a=json_data["名称"],
-                    b=并发症,
-                )
+    def create_原因类型_relation(self):
+        i = 0
+        for item in self.data:
+            i += 1
+            node1 = self.g.nodes.match(
+                "reason_type", name=item["列车组/乘客/环境/设备"]
+            ).first()
+            node2 = self.g.nodes.match("accident", index=i).first()
+            relation = Relationship(node1, "occurrence_reason_type", node2)
+            self.g.create(relation)
 
     def create_association(self):
         create_methods = [
-            self.create_疾病_to_分类,
-            self.create_疾病_to_科室,
-            self.create_疾病_to_治疗方式,
-            self.create_疾病_to_传染方式,
-            self.create_疾病_to_症状,
-            self.create_疾病_to_检查,
-            self.create_疾病_to_饮食保健,
-            self.create_疾病_to_忌吃食物,
-            self.create_疾病_to_推荐食物,
-            self.create_疾病_to_常用药品,
-            self.create_疾病_to_药品明细,
-            self.create_疾病_to_并发症,
+            self.create_time_relation,
+            self.create_route_relation,
+            self.create_place_relation,
+            self.create_checi_relation,
+            self.create_事故类型_relation,
+            self.create_详细原因_relation,
+            self.create_原因类型_relation,
         ]
         for method in create_methods:
             method()
