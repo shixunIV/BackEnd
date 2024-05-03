d162c2cbb89987b7feabba5b607d30ce5ca6475d
许一涵 <81723334@qq.com>
2024-05-03 20:05:40 +0800

    feat:爬取数据集

--- neo4jInit/data_prepare.py
+++ neo4jInit/data_prepare.py
@@ -13,12 +13,14 @@ def process_data(i, data):
     save_path = os.path.join("neo4jInit/data/new_data", f"railway_{i}.json")
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
     data[i]["列车组/乘客/环境/设备"] = ans[:2]
-    json.dump(data[i], open(save_path, "w"), ensure_ascii=False, indent=4)
+    json.dump(
+        data[i], open(save_path, "w", encoding="utf-8"), ensure_ascii=False, indent=4
+    )
 
 
 if __name__ == "__main__":
-    data = json.load(open("neo4jInit/data/railway.json"))
+    data = json.load(open("neo4jInit/data/railway.json", encoding="utf-8-sig"))
     config = read_config("./config.yml")
     gpt = GPT(config)
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
         executor.map(process_data, range(len(data)), [data] * len(data))
