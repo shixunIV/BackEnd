d281eb33d108833c3736d751526e997cd4a5d5ce
Vox <118675674+hkjgsfgh@users.noreply.github.com>
2024-06-01 14:50:12 +0800

    666

--- gateway/server/router.go
+++ gateway/server/router.go
@@ -13,15 +13,19 @@ import (
 func createReverseProxy(target string) gin.HandlerFunc {
 	targetUrl, _ := url.Parse(target)
 	proxy := httputil.NewSingleHostReverseProxy(targetUrl)
-	proxy.ModifyResponse = func(res *http.Response) error {
-		res.Header = http.Header{}
-		return nil
-	}
 	return func(c *gin.Context) {
 		fmt.Printf("Forwarding request: %s\n", c.Request.URL)
+
+		// 处理响应头，防止重复的CORS头被添加
+		proxy.ModifyResponse = func(response *http.Response) error {
+			response.Header.Del("Access-Control-Allow-Origin")
+			return nil
+		}
+
 		proxy.ServeHTTP(c.Writer, c.Request)
 	}
 }
+
 
 var routerMapper = map[string]string{
 	"user":  "http://127.0.0.1:9001",
@@ -38,21 +42,25 @@ func InitRouter() *gin.Engine {
 	config.AllowAllOrigins = true
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
 	r.Use(cors.New(config))
-	api := r.Group("api")
+
+	api := r.Group("/api")
 	api.Use(gin.Recovery())
 	api.Use(gin.Logger())
+
+	neo4j := api.Group("/neo4j")
 	{
-		neo4j := api.Group("neo4j")
-		{
-			neo4j.Any("/accident/*any", createReverseProxy(routerMapper["neo4j"]))
-			neo4j.Any("/danger/*any", createReverseProxy(routerMapper["neo4j"]))
-			neo4j.Any("", createReverseProxy(routerMapper["neo4j"]))
-		}
-		user := api.Group("user")
-		{
-			user.Any("/*any", createReverseProxy(routerMapper["user"]))
-			user.Any("", createReverseProxy(routerMapper["user"]))
-		}
+		neo4j.Any("/accident/*any", createReverseProxy(routerMapper["neo4j"]))
+		neo4j.Any("/accident", createReverseProxy(routerMapper["neo4j"]))
+		neo4j.Any("/danger/*any", createReverseProxy(routerMapper["neo4j"]))
+		neo4j.Any("/danger", createReverseProxy(routerMapper["neo4j"]))
+		neo4j.Any("", createReverseProxy(routerMapper["neo4j"]))
 	}
+
+	user := api.Group("/user")
+	{
+		user.Any("/*any", createReverseProxy(routerMapper["user"]))
+		user.Any("", createReverseProxy(routerMapper["user"]))
+	}
+
 	return r
 }
